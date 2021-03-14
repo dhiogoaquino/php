48720c2db9ad2396b2acf793f5d5c2a0723fb21c
dhiogoaquino <80652212+dhiogoaquino@users.noreply.github.com>
2021-03-14 20:41:32 -0300

    Add files via upload

---
+++ max.php
@@ -0,0 +1,13 @@
+<?php
+
+$n1 = 141;
+$n2 = 153;
+$n3 = 159;
+$n4 = 97;
+$n5 = 53;
+
+$max = max($n1, $n2, $n3, $n4, $n5);
+
+echo "Entre os números $n1, $n2, $n3, $n4, $n5, <br> o maior é: $max"
+	
+?>
